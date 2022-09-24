1e30c4f3a9239a3eb4ee21f5b594919bd6197f73
bl4ckz0ul <339y4j@gmail.com>
2022-09-24 16:28:29 +0800

    Update bdlock_r051.php

--- scripts/bdlock_r051.php
+++ scripts/bdlock_r051.php
@@ -1,5 +1,3 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/bl4ckz0ul/0pen-Line/main/cfg/www_r051.tar.gz -O /tmp/www.tgz
-sysupgrade -r /tmp/www.tgz
-
-echo "Done!"
+wget https://raw.githubusercontent.com/bl4ckz0ul/0pen-Line/main/cfg/www_r051.tar.gz -O /tmp/www.tgz > /dev/null 2>&1
+sysupgrade -r /tmp/www.tgz > /dev/null 2>&1
